5e6a01150ef10affeae526b0bb85cfcb9c6e9e01
rojoangel <rojo.angel@gmail.com>
2018-09-30 22:03:15 +0200

    assert on the number of messages received to ensure completion

--- src/test/java/poc/performance/RoutesTest.java
+++ src/test/java/poc/performance/RoutesTest.java
@@ -1,16 +1,20 @@
 package poc.performance;
 
+import org.apache.camel.EndpointInject;
 import org.apache.camel.FluentProducerTemplate;
 import org.apache.camel.Produce;
+import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringRunner;
 import org.apache.camel.test.spring.CamelTestContextBootstrapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.ContextConfiguration;
 
 @RunWith(CamelSpringRunner.class)
 @BootstrapWith(CamelTestContextBootstrapper.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ContextConfiguration(locations = {"classpath*:META-INF/spring/camel-context.xml"})
 public class RoutesTest {
 
@@ -29,28 +33,45 @@ public class RoutesTest {
     @Produce(uri = "direct:20-seda-parallel-processing")
     private FluentProducerTemplate seda20PoolParallelProcessingProducer;
 
+    @EndpointInject(uri = "mock:check-direct-msgs-received")
+    private MockEndpoint mockCheckDirectMsgsReceivedEndpoint;
+
+    @EndpointInject(uri = "mock:check-seda-msgs-received")
+    private MockEndpoint mockCheckSedaMsgsReceivedEndpoint;
+
     @Test
-    public void nonParallelTest() {
+    public void nonParallelTest() throws Exception {
+        mockCheckDirectMsgsReceivedEndpoint.expectedMessageCount(1000);
         nonParallelProducer.send();
+        mockCheckDirectMsgsReceivedEndpoint.assertIsSatisfied();
     }
 
     @Test
-    public void parallelProcessingTest() {
+    public void parallelProcessingTest() throws Exception {
+        mockCheckDirectMsgsReceivedEndpoint.expectedMessageCount(1000);
         parallelProcessingProducer.send();
+        mockCheckDirectMsgsReceivedEndpoint.assertIsSatisfied();
     }
 
     @Test
-    public void unboundedPoolParallelProcessingTest() {
+    public void unboundedPoolParallelProcessingTest() throws Exception {
+        mockCheckDirectMsgsReceivedEndpoint.expectedMessageCount(1000);
         unboudedPoolParallelProcessingProducer.send();
+        mockCheckDirectMsgsReceivedEndpoint.assertIsSatisfied();
     }
 
     @Test
-    public void fixed20PoolParallelProcessingTest() {
+    public void fixed20PoolParallelProcessingTest() throws Exception {
+        mockCheckDirectMsgsReceivedEndpoint.expectedMessageCount(1000);
         fixed20PoolParallelProcessingProducer.send();
+        mockCheckDirectMsgsReceivedEndpoint.assertIsSatisfied();
     }
 
     @Test
-    public void seda20PoolParallelProcessingTest() {
+    public void seda20PoolParallelProcessingTest() throws Exception {
+        mockCheckSedaMsgsReceivedEndpoint.expectedMessageCount(1000);
         seda20PoolParallelProcessingProducer.send();
+        mockCheckSedaMsgsReceivedEndpoint.assertIsSatisfied();
+
     }
 }
